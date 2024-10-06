ffd5aa113acd608fe772bb61d94ed6f1d4362fa1
Rahuletto <rahulmarban@gmail.com>
2024-10-06 14:45:16 +0530

    fix: fixing 404

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -2,53 +2,48 @@ import { A } from "@solidjs/router";
 
 export default function NotFound() {
   return (
-    <>
-      <head>
-        <title>404 | Reelster</title>
-      </head>
-      <main class="text-center mx-auto flex items-center flex-col justify-center gap-6 w-screen h-screen px-3">
-        <div
-          class={`flex flex-col bg-[#0e0e0e] min-h-[70vh] aspect-[9/16.65] rounded-[1.1rem] p-0.5`}
-        >
-          <div class="w-full opacity-10 h-full flex items-center justify-center flex-col gap-2">
-            <svg
-              stroke="currentColor"
-              fill="currentColor"
-              stroke-width="0"
-              viewBox="0 0 448 512"
-              height="60px"
-              width="60px"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path>
-            </svg>
-            <h1 class="text-8xl text-white font-bold uppercase my-4">404</h1>
-          </div>
-        </div>
-
-        <div
-          class={`flex transition-all duration-200 bg-[#19191b] rounded-full p-0.5 w-[90%] max-w-[350px] mx-auto`}
-        >
-          <A
-            class="bg-white p-3 w-full px-6 items-center gap-3 justify-start text-black rounded-full transition-all duration-200 flex"
-            href="/"
+    <main class="text-center mx-auto flex items-center flex-col justify-center gap-6 w-screen h-screen px-3">
+      <div
+        class={`flex flex-col bg-[#0e0e0e] min-h-[70vh] aspect-[9/16.65] rounded-[1.1rem] p-0.5`}
+      >
+        <div class="w-full opacity-10 h-full flex items-center justify-center flex-col gap-2">
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            stroke-width="0"
+            viewBox="0 0 448 512"
+            height="60px"
+            width="60px"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            <svg
-              stroke="currentColor"
-              fill="currentColor"
-              stroke-width="0"
-              viewBox="0 0 512 512"
-              height="20px"
-              width="20px"
-              class="animate-spin-reverse"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M125.7 160H176c17.7 0 32 14.3 32 32s-14.3 32-32 32H48c-17.7 0-32-14.3-32-32V64c0-17.7 14.3-32 32-32s32 14.3 32 32v51.2L97.6 97.6c87.5-87.5 229.3-87.5 316.8 0s87.5 229.3 0 316.8s-229.3 87.5-316.8 0c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0c62.5 62.5 163.8 62.5 226.3 0s62.5-163.8 0-226.3s-163.8-62.5-226.3 0L125.7 160z"></path>
-            </svg>
-            <p class="font-semibold">Rollback</p>
-          </A>
+            <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path>
+          </svg>
+          <h1 class="text-8xl text-white font-bold uppercase my-4">404</h1>
         </div>
-      </main>
-    </>
+      </div>
+
+      <div
+        class={`flex transition-all duration-200 bg-[#19191b] rounded-full p-0.5 w-[90%] max-w-[350px] mx-auto`}
+      >
+        <A
+          class="bg-white p-3 w-full px-6 items-center gap-3 justify-start text-black rounded-full transition-all duration-200 flex"
+          href="/"
+        >
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            stroke-width="0"
+            viewBox="0 0 512 512"
+            height="20px"
+            width="20px"
+            class="animate-spin-reverse"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M125.7 160H176c17.7 0 32 14.3 32 32s-14.3 32-32 32H48c-17.7 0-32-14.3-32-32V64c0-17.7 14.3-32 32-32s32 14.3 32 32v51.2L97.6 97.6c87.5-87.5 229.3-87.5 316.8 0s87.5 229.3 0 316.8s-229.3 87.5-316.8 0c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0c62.5 62.5 163.8 62.5 226.3 0s62.5-163.8 0-226.3s-163.8-62.5-226.3 0L125.7 160z"></path>
+          </svg>
+          <p class="font-semibold">Rollback</p>
+        </A>
+      </div>
+    </main>
   );
 }
