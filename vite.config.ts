32230bb8ff17e3828ac6f871ac3278e49dd9ce2a
Rahuletto <rahulmarban@gmail.com>
2024-10-06 15:27:05 +0530

    fix: try vite

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+// vite.config.ts
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
+
+export default defineConfig({
+  plugins: [solid()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: "esnext",
+  },
+});
